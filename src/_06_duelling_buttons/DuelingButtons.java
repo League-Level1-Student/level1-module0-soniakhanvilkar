975ceeb9ef0893f103b0ac7fcc224c755eb9ebec
sonia s khanvilkar <soniaskhanvilkar@gmail.com>
2021-02-26 19:58:24 -0800

    2/26/21

--- src/_06_duelling_buttons/DuelingButtons.java
+++ src/_06_duelling_buttons/DuelingButtons.java
@@ -8,6 +8,7 @@ package _06_duelling_buttons;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -27,25 +28,25 @@ public class DuelingButtons implements ActionListener {
 	public void run() {
 
 		// 1. Add the panel to the frame
-
+		frame.add(panel);
 		// 2. Make the frame visible
-
+		frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+		leftButton.setText("click me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+		rightButton.setText("click me!");
 		// 5. Add an action listener to the leftButton
-
+		leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+		rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+		panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+		panel.add(rightButton);
 		// 9. Pack the frame
-
+		frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+		frame.setTitle("Demanding Buttons");
 	}
 
 	@Override
@@ -57,9 +58,19 @@ public class DuelingButtons implements ActionListener {
 		// Set the PREFERRED size of the rightButton to BIG
 		// Set the text of the leftButton to "Click Me!"
 		// Set the PREFERRED size of the leftButton to SMALL
-
 		/* If the buttonPressed was the rightButton, do the opposite. */
-
+		
+		if (buttonPressed == leftButton) {
+			leftButton.setPreferredSize(SMALL);
+			rightButton.setPreferredSize(BIG);
+			rightButton.setText("no, click me!");
+		}
+		else {
+			rightButton.setPreferredSize(SMALL);
+			leftButton.setPreferredSize(BIG);
+			leftButton.setText("no, click me!");
+		}
+		
 		frame.pack();
 	}
 }
---
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -0,0 +1,20 @@
+package _07_fortune_cookie;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class FortuneCookie {
+    public void showButton() {
+        System.out.println("Button clicked");
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        JButton button = new JButton();
+        frame.add(button);
+        frame.pack();
+        
+        if (button == click) {
+			
+		}
+        button.addActionListener(null);
+   }
+}
